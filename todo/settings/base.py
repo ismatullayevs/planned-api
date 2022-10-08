d1b6ef6d1943c9bd4cc163bcd1444fd9e3acbc74
ismatullayevs <ismatullayevpro@gmail.com>
2022-10-08 13:05:39 +0500

    Heroku deployment

--- todo/settings/base.py
+++ todo/settings/base.py
@@ -24,13 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['192.168.1.4', 'localhost']
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
