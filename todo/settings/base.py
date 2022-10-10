aecd76c68b4f783aa4a8a1b3fdfc6fc556b08280
ismatullayevs <ismatullayevpro@gmail.com>
2022-10-10 20:57:22 +0500

    Optimized images using django-imagekit

--- todo/settings/base.py
+++ todo/settings/base.py
@@ -88,7 +88,7 @@ WSGI_APPLICATION = 'todo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
 
         # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
         # 'NAME': config('DB_NAME'),
--- users/models.py
+++ users/models.py
@@ -10,6 +10,11 @@ class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(_('Email address'), unique=True)
     avatar = models.ImageField(_('Profile picture'), upload_to='avatars', blank=True, null=True)
+    avatar_thumbnail = ImageSpecField(source='avatar',
+                                      processors=[ResizeToFill(100, 100)],
+                                      format='JPEG',
+                                      options={'quality': 50})
+
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
