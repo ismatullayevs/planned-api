74e87d02d03579074628b3b8ef06f55d8864bed6
Javohir Ismatullayev <70445308+ismatullayevs@users.noreply.github.com>
2023-09-30 13:17:48 +0500

    Remove localhost from redirect uris

--- todo/settings/base.py
+++ todo/settings/base.py
@@ -194,7 +194,7 @@ DJOSER = {
     'USERNAME_CHANGED_EMAIL_CONFIRMATION': True,
     'PASSWORD_CHANGED_EMAIL_CONFIRMATION': True,
     'SOCIAL_AUTH_TOKEN_STRATEGY': 'djoser.social.token.jwt.TokenStrategy',
-    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ['http://localhost:3000/login', 'https://ismatullayevs.github.io/planned/login'],
+    'SOCIAL_AUTH_ALLOWED_REDIRECT_URIS': ['https://ismatullayevs.github.io/planned/login'],
 }
 
 
